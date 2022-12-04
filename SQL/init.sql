826de74a407926cd485607988b5e53ed3f25792b
Kate <katefa46@gmail.com>
2022-12-04 13:41:56 -0500

    Create tables

--- SQL/init.sql
+++ SQL/init.sql
@@ -1,15 +1,128 @@
 DROP DATABASE IF EXISTS "LookInnaBook";
 CREATE DATABASE "LookInnaBook";
 
+CREATE TABLE PUBLISHER (
+   publisher_id CHAR(10) NOT NULL PRIMARY KEY,
+   name VARCHAR(20) NOT NULL,
+   email VARCHAR(30) UNIQUE NOT NULL,
+   phone_number NUMERIC(10,0) UNIQUE,
+   bank_acc_num NUMERIC(17,0) NOT NULL
+);
+
 CREATE TABLE BOOK (
     isbn NUMERIC(13,0) NOT NULL PRIMARY KEY,
     title VARCHAR NOT NULL,
     page_count INT,
-    price NUMERIC(4,2),
+    price NUMERIC(4,2) NOT NULL,
     num_sold INT,
     num_sold_last_month INT,
-    percent_for_publisher NUMERIC(3,2),
-    wholesale_price NUMERIC(4,2),
-    publisher_id CHAR(10)
-)
+    percent_for_publisher NUMERIC(3,2) NOT NULL,
+    wholesale_price NUMERIC(4,2) NOT NULL,
+    publisher_id CHAR(10) NOT NULL,
+    FOREIGN KEY (publisher_id)
+                  REFERENCES Publisher(publisher_id)
+);
 
+CREATE TABLE ADDRESS (
+    street_num INT NOT NULL,
+    street VARCHAR(20) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    province CHAR(2) NOT NULL,
+    country VARCHAR(30) NOT NULL,
+    postal_code CHAR(6) NOT NULL,
+    PRIMARY KEY (street_num, street, postal_code)
+);
+
+CREATE TABLE ORDERS (
+    order_num NUMERIC(5,0) NOT NULL PRIMARY KEY,
+    postal_code CHAR(6) NOT NULL,
+    street VARCHAR(20) NOT NULL,
+    street_num INT NOT NULL,
+    current_city VARCHAR(20) NOT NULL,
+    eta DATE,
+    FOREIGN KEY (postal_code, street, street_num)
+                REFERENCES Address(postal_code, street, street_num)
+);
+
+CREATE TABLE AUTHOR (
+    author_id CHAR(10) NOT NULL PRIMARY KEY,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE CREDIT_CARD (
+    card_num NUMERIC(16,0) NOT NULL PRIMARY KEY,
+    expiry_date DATE NOT NULL,
+    cvv NUMERIC(3,0) NOT NULL
+);
+
+CREATE TABLE ACCOUNT (
+    username VARCHAR(20) NOT NULL PRIMARY KEY,
+    password VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE GENRE (
+    name VARCHAR(20) NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE WRITTEN_BY (
+    book_isbn NUMERIC(13,0) NOT NULL,
+    author_id CHAR(10) NOT NULL,
+    PRIMARY KEY (book_isbn, author_id),
+    FOREIGN KEY (book_isbn)
+                REFERENCES Book(isbn),
+    FOREIGN KEY (author_id)
+                REFERENCES Author(author_id)
+);
+
+CREATE TABLE HAS_GENRE (
+    book_isbn NUMERIC(13,0) NOT NULL,
+    genre VARCHAR(20) NOT NULL,
+    PRIMARY KEY (book_isbn, genre),
+    FOREIGN KEY (genre)
+                REFERENCES Genre(name),
+    FOREIGN KEY (book_isbn)
+                REFERENCES Book(isbn)
+);
+
+CREATE TABLE INCLUDES (
+    order_num NUMERIC(5,0) NOT NULL,
+    book_isbn NUMERIC(13,0) NOT NULL,
+    PRIMARY KEY (order_num, book_isbn),
+    FOREIGN KEY (order_num)
+                REFERENCES Orders(order_num),
+    FOREIGN KEY (book_isbn)
+                REFERENCES Book(isbn)
+);
+
+CREATE TABLE PAID_WITH (
+    card_num NUMERIC(16,0) NOT NULL,
+    order_num NUMERIC(5,0) NOT NULL,
+    PRIMARY KEY (card_num, order_num),
+    FOREIGN KEY (card_num)
+                REFERENCES Credit_Card(card_num),
+    FOREIGN KEY (order_num)
+                REFERENCES Orders(order_num)
+);
+
+CREATE TABLE CHARGES_TO (
+    account_name VARCHAR(20) NOT NULL,
+    credit_card NUMERIC(16,0) NOT NULL,
+    PRIMARY KEY (account_name, credit_card),
+    FOREIGN KEY (account_name)
+                REFERENCES Account(username),
+    FOREIGN KEY (credit_card)
+                REFERENCES Credit_Card(card_num)
+);
+
+CREATE TABLE SHIPS_TO (
+    account_name VARCHAR(20) NOT NULL,
+    postal_code CHAR(6) NOT NULL,
+    street VARCHAR(20) NOT NULL,
+    street_num INT NOT NULL,
+    PRIMARY KEY (account_name, postal_code, street, street_num),
+    FOREIGN KEY (account_name)
+                REFERENCES Account(username),
+    FOREIGN KEY (postal_code, street, street_num)
+                REFERENCES Address(postal_code, street, street_num)
+);
--- business_logic.py
+++ business_logic.py
@@ -8,12 +8,54 @@ def execute_query(cur, query):
     except OperationalError as e:
         print("ERROR:\n", e)
 
+
 def create_tables(cur):
     f = open("./SQL/init.sql", "r").read().split("\n")
 
-    book_query = "".join(f[3:15])
+    publisher_query = "".join(f[3:10])
+    execute_query(cur, publisher_query)
 
+    book_query = "".join(f[11:24])
     execute_query(cur, book_query)
+
+    address_query = "".join(f[25:34])
+    execute_query(cur, address_query)
+
+    order_query = "".join(f[35:45])
+    execute_query(cur, order_query)
+
+    author_query = "".join(f[46:51])
+    execute_query(cur, author_query)
+
+    credit_card_query = "".join(f[52:57])
+    execute_query(cur, credit_card_query)
+
+    account_query = "".join(f[58:62])
+    execute_query(cur, account_query)
+
+    genre_query = "".join(f[63:66])
+    execute_query(cur, genre_query)
+
+    written_by_query = "".join(f[67:76])
+    execute_query(cur, written_by_query)
+
+    has_genre_query = "".join(f[77:86])
+    execute_query(cur, has_genre_query)
+
+    includes_query = "".join(f[87:96])
+    execute_query(cur, includes_query)
+
+    paid_with_query = "".join(f[97:106])
+    execute_query(cur, paid_with_query)
+
+    charges_to_query = "".join(f[107:116])
+    execute_query(cur, charges_to_query)
+
+    ships_to_query = "".join(f[117:])
+    execute_query(cur, ships_to_query)
+
+
+
 
 def create_database():
     con = psycopg2.connect(
